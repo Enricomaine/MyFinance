61deada0253bc8a3a3e2acf6417e93adcf1e828c
enricomaine <enricomaine@gmail.com>
2023-06-14 22:22:53 -0300

    Criacao do metodo de criacao de lancamento

--- src/Operacoes/Lancamento.java
+++ src/Operacoes/Lancamento.java
@@ -4,10 +4,12 @@ import Cadastros.CaixaTransacao;
 
 import javax.swing.*;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Lancamento {
 
@@ -18,7 +20,8 @@ public class Lancamento {
     private double valorEntrada;
 
     CaixaTransacao caixaTransacao = new CaixaTransacao();
-    public void criaLancamento(Lancamento lancamento, Connection c) throws ParseException {
+    public void criaLancamento(Lancamento lancamento, Connection c) {
+
         idTransacao = Integer.parseInt(JOptionPane.showInputDialog("Selecione a transacao: "));
         if (caixaTransacao.getEntradaSaida(idTransacao, c) == "1") {
             valorEntrada = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor da entrada: "));
@@ -26,8 +29,34 @@ public class Lancamento {
             valorSaida = Double.parseDouble((JOptionPane.showInputDialog("Informe o valor da saida: ")));
         }
         data = JOptionPane.showInputDialog("Informe a data do lancamento: ", "  /  /    ");
-        Date data = (Date) new SimpleDateFormat("dd/MM/yyyy").parse(lancamento.data);
-        System.out.println(data);
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date inputData = null;
+
+        try {
+            inputData = dateFormat.parse(data);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        PreparedStatement ps = null;
+        String queryEntrada = "INSERT INTO myfinance.caixa (idtransacao, data, valorentrada) VALUES (?, ?, ?)";
+        String querySaida = "INSERT INTO myfinance.caixa (idtransacao, data, valorsaida) VALUES (?, ?, ?)";
+        try {
+            if (caixaTransacao.getEntradaSaida(idTransacao, c) == "1") {
+                ps = c.prepareStatement(queryEntrada);
+                ps.setDouble(3, valorEntrada);
+            } else {
+                ps = c.prepareStatement(querySaida);
+                ps.setDouble(3, valorSaida);
+            }
+            ps.setInt(1, idTransacao);
+            ps.setDate(2, new java.sql.Date(inputData.getTime()));
+            ps.execute();
+            ps.close();
+            System.out.println("Lancamento inserido");
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir registro: "+e.getMessage());
+        }
     }
 
     public void editaLancamento() {
